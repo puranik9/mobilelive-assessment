c9b907624fdabf70d9d3c92d74724b941c29d37d
Kartik Karkala <kartik.karkala@mail.mcgill.ca>
2019-02-11 20:24:33 -0500

    Updates

---
+++ precache-manifest.28a4e767c350379ca813efd852df29f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mobilelive-assessment/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3fedc3efa1e8aef15b0b",
+    "url": "/mobilelive-assessment/static/js/runtime~main.3fedc3ef.js"
+  },
+  {
+    "revision": "525fe4d5794d2d019ca1",
+    "url": "/mobilelive-assessment/static/js/main.525fe4d5.chunk.js"
+  },
+  {
+    "revision": "7bbf2c8e29b4c57bc3d8",
+    "url": "/mobilelive-assessment/static/js/2.7bbf2c8e.chunk.js"
+  },
+  {
+    "revision": "525fe4d5794d2d019ca1",
+    "url": "/mobilelive-assessment/static/css/main.c21ef8c9.chunk.css"
+  },
+  {
+    "revision": "88cc0a37ad1f2b984be27ef99a8cba23",
+    "url": "/mobilelive-assessment/index.html"
+  }
+];
